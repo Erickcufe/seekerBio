e590369f8aa110933ebaf9dbd1e5ebf9eb1e8c28
Erick Cuevas <erickcufe94@gmail.com>
2019-08-07 09:13:54 -0500

    Fix "gen1 x gen2"

--- R/seeker_gwas.R
+++ R/seeker_gwas.R
@@ -40,16 +40,16 @@ seeker_gwas.character <- function(trait){
       print("two words")
       # trait <- "multiple sclerosis"
 
-      keyword_GWAS<-trait[[1]][1]
-      keyword_2GWAS<-trait[[1]][2]
+      keyword_GWAS <- trait[[1]][1]
+      keyword_2GWAS <- trait[[1]][2]
 
-      url_GWAS<-paste0("https://www.ebi.ac.uk/gwas/api/search/downloads?q=text:%22",
+      url_GWAS <- paste0("https://www.ebi.ac.uk/gwas/api/search/downloads?q=text:%22",
                        keyword_GWAS,"%20",keyword_2GWAS,
                        "%22&pvalfilter=&orfilter=&betafilter=&datefilter=&genomicfilter=&traitfilter[]=&genotypingfilter[]=&dateaddedfilter=&efo=true&facet=association")
 
-      GWAS<-read.delim(url_GWAS)
-      separarcoma<-str_split(GWAS$REPORTED.GENE.S., pattern = fixed(","), n = Inf)
-      separarcoma<-as.matrix(separarcoma)
+      GWAS <- read.delim(url_GWAS)
+      separarcoma <- stringr::str_split(GWAS$REPORTED.GENE.S., pattern = stringr::fixed(","), n = Inf)
+      separarcoma <- as.matrix(separarcoma)
 
       p_separar <- sapply(separarcoma, length)
       o_separar <- seq_len(max(p_separar))
@@ -58,20 +58,33 @@ seeker_gwas.character <- function(trait){
       names(k_separar)[1:length(k_separar)] <- c("GeneSymbol")
 
 
-      binding<-data.frame(k_separar[,1])
-      colnames(binding)<-"GeneSymbol"
-      ENSEMBL_GWAS<-cbind(binding, GWAS)
+      binding <- data.frame(k_separar[,1])
+
+      separarcoma_2 <- stringr::str_split(binding$k_separar...1., pattern = stringr::fixed(" "), n = Inf)
+      separarcoma_2 <- as.matrix(separarcoma_2)
+
+      p_separar_2 <- sapply(separarcoma_2, length)
+      o_separar_2 <- seq_len(max(p_separar_2))
+      k_separar_2 <- t(sapply(separarcoma_2, "[", i=o_separar))
+      k_separar_2 <- data.frame(k_separar_2, headers=F)
+      names(k_separar_2)[1:length(k_separar_2)] <- c("GeneSymbol")
+      binding<-data.frame(k_separar_2[,1])
+
+      colnames(binding_2) <- "GeneSymbol"
+      ENSEMBL_GWAS <- cbind(binding_2, GWAS)
+
+
     } else {
 
       if (is.na(trait[[1]][2])){
 
         print("one_word")
-        url_GWAS<-paste0("https://www.ebi.ac.uk/gwas/api/search/downloads?q=text:%22",
+        url_GWAS <- paste0("https://www.ebi.ac.uk/gwas/api/search/downloads?q=text:%22",
                          trait,
                          "%22&pvalfilter=&orfilter=&betafilter=&datefilter=&genomicfilter=&traitfilter[]=&genotypingfilter[]=&dateaddedfilter=&efo=true&facet=association")
-        GWAS<-read.delim(url_GWAS)
-        separarcoma<-str_split(GWAS$REPORTED.GENE.S., pattern = fixed(","), n = Inf)
-        separarcoma<-as.matrix(separarcoma)
+        GWAS <- read.delim(url_GWAS)
+        separarcoma <- stringr::str_split(GWAS$REPORTED.GENE.S., pattern = stringr::fixed(","), n = Inf)
+        separarcoma <- as.matrix(separarcoma)
 
         p_separar <- sapply(separarcoma, length)
         o_separar <- seq_len(max(p_separar))
@@ -80,9 +93,21 @@ seeker_gwas.character <- function(trait){
         names(k_separar)[1:length(k_separar)] <- c("GeneSymbol")
 
 
-        binding<-data.frame(k_separar[,1])
-        colnames(binding)<-"GeneSymbol"
-        ENSEMBL_GWAS<-cbind(binding, GWAS)
+        binding <- data.frame(k_separar[,1])
+
+
+        separarcoma_2 <- stringr::str_split(binding$k_separar...1., pattern = stringr::fixed(" "), n = Inf)
+        separarcoma_2 <- as.matrix(separarcoma_2)
+
+        p_separar_2 <- sapply(separarcoma_2, length)
+        o_separar_2 <- seq_len(max(p_separar_2))
+        k_separar_2 <- t(sapply(separarcoma_2, "[", i=o_separar))
+        k_separar_2 <- data.frame(k_separar_2, headers=F)
+        names(k_separar_2)[1:length(k_separar_2)] <- c("GeneSymbol")
+        binding_2 <- data.frame(k_separar_2[,1])
+
+        colnames(binding_2) <- "GeneSymbol"
+        ENSEMBL_GWAS <- cbind(binding_2, GWAS)
 
       }
     }
