020fd9298963209aaec004d9386c61299216d40d
Erick Cuevas <erickcufe94@gmail.com>
2020-02-05 19:08:31 -0600

    .

--- data-raw/HouseKeeping.R
+++ data-raw/HouseKeeping.R
@@ -1,3 +1,6 @@
 ## code to prepare `HouseKeeping` dataset goes here
 
-usethis::use_data("HouseKeeping")
+HouseKeeping <- read.delim("data-raw/HouseKeeping_final.txt", header = FALSE)
+colnames(HouseKeeping) <- c("Gen", "ID")
+
+# usethis::use_data(HouseKeeping)
